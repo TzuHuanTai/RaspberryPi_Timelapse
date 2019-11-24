0083df9b36949330df3dd4f37804b00427ecd45b
Tzy-Huan, Tai <andy81719@gmail.com>
2019-11-24 15:30:29 +0800

    Set default folder bu $(pwd)

--- Timelapse/capturePhoto.sh
+++ Timelapse/capturePhoto.sh
@@ -11,14 +11,14 @@
 FolderName=`date +%Y%m%d`;
 
 # 建立資料夾，帶-p表示資料夾存在的話忽略，不存在則建立
-mkdir -p /home/pi/Timelapse/photo/$FolderName
+mkdir -p $(pwd)/photo/$FolderName
 
 # 已當下日期命名圖片檔
 DATE=`date +%Y%m%d%H%M`;
 
 # 透過fswebcam截圖，先skip 60張圖讓webcam先自動對焦完成
 # 將banner設定成透明，並加入時間戳記與調整字型
-fswebcam -r 1280x720 -S 60 --banner-colour '#FF000000' --line-colour '#FF000000' --timestamp '%Y-%m-%d %H:%M' --font 'sans:32' /home/pi/Timelapse/photo/$FolderName/$DATE.jpg
+fswebcam -r 1280x720 -S 60 --banner-colour '#FF000000' --line-colour '#FF000000' --timestamp '%Y-%m-%d %H:%M' --font 'sans:32' $(pwd)/photo/$FolderName/$DATE.jpg
 
 # return 0 to system
 exit 0
